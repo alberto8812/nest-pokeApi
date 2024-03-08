96f078d0ee6085b7b325b48bb65d796ad3a25fa1
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-07 22:17:02 -0500

    update pokemin with validation

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -26,7 +26,7 @@ export class PokemonController {
 
   @Patch(':term')
   update(@Param('term') term: string, @Body() updatePokemonDto: UpdatePokemonDto) {
-    return this.pokemonService.update(+term, updatePokemonDto);
+    return this.pokemonService.update(term, updatePokemonDto);
   }
 
   @Delete(':id')
