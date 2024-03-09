f4ac4ffbcac84bf6d2916c1a2929802b901d5722
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-08 19:03:16 -0500

    crear archivos seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 
 @Injectable()
 export class SeedService {
   //para que no cree una dependencia oculta
   private readonly axios: AxiosInstance =axios;
+  constructor(
+   @InjectModel(Pokemon.name)
+   private readonly pokemonModel: Model<Pokemon>, //model de mong, y el generico de la entity
+ ) {}
 
 
  async executeSeed(){
-  const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1');
-  data.results.forEach(({name,url}) => {
+  const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=16');
+  data.results.forEach(async({name,url}) => {
      const no=+url.at(-2);
-     console.log({name,no})
+     const pokemon = await this.pokemonModel.create({name,no})
      
   });
 
-  return data;
+  return 'seed ejecuatdo';
  }
 }
