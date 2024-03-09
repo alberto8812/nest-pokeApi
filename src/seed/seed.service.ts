720f935e4fb3b6caf11811defdbbb6faf4be9a8b
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-09 15:29:37 -0500

    use adapter for axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 
 @Injectable()
 export class SeedService {
   //para que no cree una dependencia oculta
-  private readonly axios: AxiosInstance =axios;
+  private readonly http:AxiosAdapter
   constructor(
    @InjectModel(Pokemon.name)
    private readonly pokemonModel: Model<Pokemon>, //model de mong, y el generico de la entity
@@ -20,7 +20,7 @@ export class SeedService {
 
   await this.pokemonModel.deleteMany({});
 
-  const {data}= await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+  const data= await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
   const PokemonToinsert:{name:string,no:number}[]=[];
   data.results.forEach(async({name,url}) => {
      const segments=url.split('/');
