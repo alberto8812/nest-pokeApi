509b907f8422d820f9da156202b0e28ef364dfe9
alberto8812 <101751073+alberto8812@users.noreply.github.com>
2024-03-10 10:54:16 -0500

    install Joi to validate squema  env

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,19 +4,27 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
-import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
+
+  private readonly defaultLimit:number;
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>, //model de mong, y el generico de la entity
-  ) {}
+    private readonly configService:ConfigService
+  ) {
+   this.defaultLimit= this.configService.get<number>('defaultLimit');
+
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -41,7 +49,7 @@ export class PokemonService {
    findAll(paginationDto:PaginationDto) {
     const {limit=10,offset=0}=paginationDto;
     return this.pokemonModel.find()
-    .limit(limit)
+    .limit(this.defaultLimit)
     .skip(offset)
     .sort({
       no:1
